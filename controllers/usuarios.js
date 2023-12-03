b6ee64c2cf20d6c91e837ffb9f7cb2931d0b50d5
LauraGL <laugranadolopez@gmail.com>
2023-12-03 19:52:15 +0100

    Cambios para admin usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,50 +1,50 @@
-const{request, response} = require("express");
-const Usuario = require ("../models/usuario");
-const bcrypt = require ("bcryptjs");
+const { request, response } = require("express");
+const Usuario = require("../models/usuario");
+const bcrypt = require("bcryptjs");
 
+const usuariosGet = async (req = request, res = response) => {
+  const { limite = 10, desde = 0 } = req.query;
 
+  //const usuarios= await Usuario.find().limit(limite).skip(desde);
+  //const total= await Usuario.countDocuments();
+  console.log("usuriosGet", request);
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments({ state: true }),
+    Usuario.find().limit(limite).skip(desde),
+    // { state: true }
+  ]);
+  res.status(200).json({
+    total,
+    usuarios,
+  });
+};
 
-const usuariosGet = async (req=request, res=response) => {
-    const {limite = 10 ,desde = 0} = req.query;
-    
-    //const usuarios= await Usuario.find().limit(limite).skip(desde);
-    //const total= await Usuario.countDocuments();
-    console.log("usuriosGet",request);
-    const[total,usuarios] = await Promise.all([
-      Usuario.countDocuments({state:true}),
-      Usuario.find({state:true}).limit(limite).skip(desde),  
-    ])
-    res.status(200).json({
-     total,   
-     usuarios,
-    });
-   }
+const usuarioPost = async (req = request, res) => {
+  const { name, email, dni, password, role } = req.body;
+  const usuario = new Usuario({ name, email, dni, password, role });
 
-const usuarioPost = async(req=request, res) => {
-    
-    const {name,email,dni,password,role}=req.body;
-    const usuario = new Usuario({ name, email,dni, password,role});
+  console.log("usuarioPost", request);
+  const salt = bcrypt.genSaltSync();
+  usuario.password = bcrypt.hashSync(password, salt);
+  usuario.role = "USER_ROLE";
 
-    console.log("usuarioPost", request);
-    const salt = bcrypt.genSaltSync();
-    usuario.password=bcrypt.hashSync(password,salt);
-    
+  await usuario.save();
 
-    await usuario.save();
-
-    res.status(201).json({
-     message:"Usuario creado",
-     usuario, //toJSON
-    });
-   };
+  res.status(201).json({
+    message: "Usuario creado",
+    usuario, //toJSON
+  });
+};
 
 const usuarioPut = async (req = request, res) => {
   const { id } = req.params;
 
   const { password, _id, l, ...resto } = req.body;
 
-  const salt = bcrypt.genSaltSync();
-  resto.password = bcrypt.hashSync(password, salt);
+  if (password != null) {
+    const salt = bcrypt.genSaltSync();
+    resto.password = bcrypt.hashSync(password, salt);
+  }
 
   const usuario = await Usuario.findByIdAndUpdate(id, resto, { new: true });
 
@@ -55,31 +55,31 @@ const usuarioPut = async (req = request, res) => {
 };
 
 const usuarioDelete = async (req, res) => {
-    const{id} = req.params;
+  const { id } = req.params;
 
-    //const usuarioBorrado = await Usuario.findByIdAndDelete(id);
-    const usuarioBorrado = await Usuario.findByIdAndUpdate(id,{
-        state:false},{new:true}
-    );
-    res.status(200).json({
-     message:"Usuario eliminado",
-     usuarioBorrado,
-    });
-   };
-/* const getUserById= async(req=request,res=response)=>{
-       const{id}=req.params;
-       const user=await Usuario.findById(id);
-       res.json({user});
-}; 
-    */
+  //const usuarioBorrado = await Usuario.findByIdAndDelete(id);
+  const usuarioBorrado = await Usuario.findByIdAndUpdate(
+    id,
+    {
+      state: false,
+    },
+    { new: true }
+  );
+  res.status(200).json({
+    message: "Usuario eliminado",
+    usuarioBorrado,
+  });
+};
+const getUserById = async (req = request, res = response) => {
+  const { id } = req.params;
+  const user = await Usuario.findById(id);
+  res.json({ user });
+};
 
-   module.exports={
-    usuariosGet,
-    usuarioPost,
-    usuarioPut,
-    usuarioDelete,
-    //getUserById
-   }
-
-
-
+module.exports = {
+  usuariosGet,
+  usuarioPost,
+  usuarioPut,
+  usuarioDelete,
+  getUserById,
+};
